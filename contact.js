536dd5b92716fe9bbd40c0bca7902dc8b64d2da6
Tamales :) <tamaran571@stemcollegiate.ca>
2024-05-26 23:43:18 -0600

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() {
+    alert("Your message was successfully sent!");
+}
